3a273965cb547520bc9948171ea1521bc1b07a22
dlob6 <32086962+dlob6@users.noreply.github.com>
2019-12-27 09:59:23 +0100

    Update download_yolov3_weights.sh

--- weights/download_yolov3_weights.sh
+++ weights/download_yolov3_weights.sh
@@ -4,17 +4,4 @@
 mkdir -p weights && cd weights
 
 # copy darknet weight files, continue '-c' if partially downloaded
-wget -c https://pjreddie.com/media/files/yolov3.weights
-wget -c https://pjreddie.com/media/files/yolov3-tiny.weights
-wget -c https://pjreddie.com/media/files/yolov3-spp.weights
-
-# yolov3 pytorch weights
-# download from Google Drive: https://drive.google.com/drive/folders/1uxgUBemJVw9wZsdpboYbzUN4bcRhsuAI
-
-# darknet53 weights (first 75 layers only)
-wget -c https://pjreddie.com/media/files/darknet53.conv.74
-
-# yolov3-tiny weights from darknet (first 16 layers only)
-# ./darknet partial cfg/yolov3-tiny.cfg yolov3-tiny.weights yolov3-tiny.conv.15 15
-# mv yolov3-tiny.conv.15 ../
-
+wget -c https://drive.google.com/open?id=1JpkINv9ISpkydSsHE3osL71ed7g8D_Py
